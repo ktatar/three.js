73105518e992a6ed5cec0f6aa543b33fe7effb58
Paul <paulparoczai@gmail.com>
2021-09-01 16:26:43 -0700

    brute force sequencer

--- examples/jsm/audio/AudioSequencer.js
+++ examples/jsm/audio/AudioSequencer.js
@@ -7,6 +7,32 @@ class AudioSequencer extends Object3D {
 		super();
 
 		this.type = 'AudioSequencer';
+		this.sequence = [];
+
+	}
+
+	addSequence( idx , array ){
+
+		this.sequence[idx] = array;
+
+	}
+
+	outputSequence(){
+
+		const subLength = this.sequence[0].length;
+		let outputSequence = [];
+
+		for( let i = 0 ; i < subLength ; i++ ){
+
+			for( let j = 0 ; j < this.sequence.length ; j++ ){
+
+				outputSequence[j] = this.sequence[j][i];
+
+			}
+
+			console.log( outputSequence );
+
+		}
 
 	}
 
