d8433b40cbc786aaa5884a2ede35ff49a2076ed1
paparoc <paulparoczai@gmail.com>
2021-10-01 11:44:06 -0700

    pre-pull commit

--- examples/jsm/audio/SynthPresets.js
+++ examples/jsm/audio/SynthPresets.js
@@ -1,6 +1,7 @@
 import { Object3D } from '../../../build/three.module.js';
 import { AudioBuffer } from './AudioBuffer.js'; 
 import { AudioGenerator } from './AudioGenerator.js'; 
+import { AudioFX } from './AudioFX.js'; 
 
 class SynthPresets extends Object3D {
 
@@ -36,6 +37,24 @@ class SynthPresets extends Object3D {
 	p4_amBuffer(){
 
 		this.preset = new Preset4_AMBuffer( this );
+
+	}
+
+	p5_amChordPad(){
+
+		this.preset = new Preset5_AMChordPad( this );
+
+	}
+
+	p6_amChordKey(){
+
+		this.preset = new Preset6_AMChordKey( this );
+
+	}
+
+	p7_noiseHit(){
+
+		this.preset = new Preset7_NoiseHit( this );
 
 	}
 
@@ -177,6 +196,217 @@ class Preset4_AMBuffer{
 		
 		this.synthGenerator.connect( this.output );
 		this.output.connect( this.listener.getInput() );
+
+	}
+
+	start(){
+
+		this.synthGenerator.start();
+
+	}
+
+}
+
+class Preset5_AMChordPad{
+
+	constructor( synthPresets ){
+
+		this.listener = synthPresets.listener;
+		this.context = synthPresets.context;
+
+		// SYNTH BUFFER
+
+			const fundamentalFrequency = 432;
+			const octaveArray = [ 1 , 2 , 4 ];
+			const intervalArray = [ 1 , 9/8 , 5/4 , 4/3 , 5/3 ];
+			const numberOfVoices = 10;
+			let carrierFrequency = 0;
+			let octave = 0;
+			let interval = 0;
+			let modulatorFrequency = 0;
+
+			this.synthBuffer = new AudioBuffer( this.listener );
+			this.synthBuffer.createBuffer( 1 , 2 );
+
+
+			for( let i = 0 ; i < numberOfVoices ; i++ ){
+
+				octave = octaveArray[ Math.floor( Math.random() * octaveArray.length ) ];
+				interval = intervalArray[ Math.floor ( Math.random() * intervalArray.length ) ];
+
+				carrierFrequency = fundamentalFrequency * octave * interval;
+
+				modulatorFrequency = 1 + Math.floor( Math.random() * 10 );
+
+				this.synthBuffer.am( carrierFrequency , modulatorFrequency , 1 ).add( 0 );
+
+			}
+
+
+			this.synthBuffer.ramp( 0 , 1 , 0.5 , 0.5 , 1 , 1 ).multiply( 0 );
+
+		// SYNTH GENERATOR
+
+			this.synthGenerator = new AudioGenerator( this.listener );
+			this.synthGenerator.buffer = this.synthBuffer.buffer;
+			this.synthGenerator.playbackRate = 1;
+
+		// EFFECTS
+
+			// REVERB
+				
+				this.reverbBuffer = new AudioBuffer( this.listener );
+				this.reverbBuffer.createBuffer( 2 , 3 );
+				this.reverbBuffer.noise().add( 0 );
+				this.reverbBuffer.noise().add( 1 );
+				this.reverbBuffer.ramp( 0 , 1 , 0.01 , 0.015 , 0.1 , 4 ).multiply( 0 );
+				this.reverbBuffer.ramp( 0 , 1 , 0.01 , 0.015 , 0.1 , 4 ).multiply( 1 );
+
+				this.reverbEffect = new AudioFX( this.listener );
+				this.reverbEffect.convolver( this.reverbBuffer.buffer );
+
+				this.reverbEffect.init();
+				
+
+		// OUTPUT GAIN
+
+			this.output = this.context.createGain();
+			this.output.gain.value = 0.5 * 1 / numberOfVoices;
+
+		// CONNECTIONS
+
+			this.synthGenerator.connect( this.reverbEffect.input );
+
+			this.synthGenerator.connect( this.output );
+			this.reverbEffect.connect( this.output );
+
+			this.output.connect( this.listener.getInput() );
+
+	}
+
+	start(){
+
+		this.synthGenerator.start();
+
+	}
+
+}
+
+class Preset6_AMChordKey{
+
+	constructor( synthPresets ){
+
+		this.listener = synthPresets.listener;
+		this.context = synthPresets.context;
+
+		// SYNTH BUFFER
+
+			const fundamentalFrequency = 432 * 2;
+			const octaveArray = [ 1 , 2 , 4 ];
+			const intervalArray = [ 1 , 9/8 , 5/4 , 4/3 , 5/3 ];
+			const numberOfVoices = 10;
+			let carrierFrequency = 0;
+			let octave = 0;
+			let interval = 0;
+			let modulatorFrequency = 0;
+
+			this.synthBuffer = new AudioBuffer( this.listener );
+			this.synthBuffer.createBuffer( 1 , 2 );
+
+
+			for( let i = 0 ; i < numberOfVoices ; i++ ){
+
+				octave = octaveArray[ Math.floor( Math.random() * octaveArray.length ) ];
+				interval = intervalArray[ Math.floor ( Math.random() * intervalArray.length ) ];
+
+				carrierFrequency = fundamentalFrequency * octave * interval;
+
+				modulatorFrequency = 1 + Math.floor( Math.random() * 10 );
+
+				this.synthBuffer.am( carrierFrequency , modulatorFrequency , 1 ).add( 0 );
+
+			}
+
+
+			this.synthBuffer.ramp( 0 , 1 , 0.01 , 0.015 , 0.1 , 4 ).multiply( 0 );
+
+		// SYNTH GENERATOR
+
+			this.synthGenerator = new AudioGenerator( this.listener );
+			this.synthGenerator.buffer = this.synthBuffer.buffer;
+			this.synthGenerator.playbackRate = 1;
+
+		// EFFECTS
+
+			// REVERB
+				
+				this.reverbBuffer = new AudioBuffer( this.listener );
+				this.reverbBuffer.createBuffer( 2 , 3 );
+				this.reverbBuffer.noise().add( 0 );
+				this.reverbBuffer.noise().add( 1 );
+				this.reverbBuffer.ramp( 0 , 1 , 0.01 , 0.015 , 0.1 , 4 ).multiply( 0 );
+				this.reverbBuffer.ramp( 0 , 1 , 0.01 , 0.015 , 0.1 , 4 ).multiply( 1 );
+
+				this.reverbEffect = new AudioFX( this.listener );
+				this.reverbEffect.convolver( this.reverbBuffer.buffer );
+
+				this.reverbEffect.init();
+				
+
+		// OUTPUT GAIN
+
+			this.output = this.context.createGain();
+			this.output.gain.value = 0.5 * 1 / numberOfVoices;
+
+		// CONNECTIONS
+
+			this.synthGenerator.connect( this.reverbEffect.input );
+
+			this.synthGenerator.connect( this.output );
+			this.reverbEffect.connect( this.output );
+
+			this.output.connect( this.listener.getInput() );
+
+	}
+
+	start(){
+
+		this.synthGenerator.start();
+
+	}
+
+}
+
+class Preset7_NoiseHit{
+
+	constructor( synthPresets ){
+
+		this.listener = synthPresets.listener;
+		this.context = synthPresets.context;
+
+		// SYNTH BUFFER
+
+			this.synthBuffer = new AudioBuffer( this.listener );
+			this.synthBuffer.createBuffer( 1 , 0.25 );
+			this.synthBuffer.noise().fill( 0 );
+			this.synthBuffer.ramp( 0 , 1 , 0.01 , 0.015 , 0.1 , 4 ).multiply( 0 );
+
+		// SYNTH GENERATOR
+
+			this.synthGenerator = new AudioGenerator( this.listener );
+			this.synthGenerator.buffer = this.synthBuffer.buffer;
+			this.synthGenerator.playbackRate = 1;
+		
+
+		// OUTPUT GAIN
+
+			this.output = this.context.createGain();
+			this.output.gain.value = 0.125;
+
+		// CONNECTIONS
+
+			this.synthGenerator.connect( this.output );
+			this.output.connect( this.listener.getInput() );
 
 	}
 
